b59dceda550fa553b95c8c504e0cafe625d697a9
alok-kiran <alok@invygo.com>
2021-01-25 23:58:25 +0400

    setup the project

--- App.js
+++ App.js
@@ -1,76 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  ScrollView
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
+class App extends React.Component {
+  navigationOptions = {
+    title: "Timer"
+  }
+  render(){
+    return (
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          <ScrollView style={styles.timerList}>
+            <ToggleableTimerForm/> 
+            <EditableTimer
+              id="1"
+              title="Mow the lawn"
+              project="House Chores"
+              elapsed="8986300"
+              isRunning
+            />
+            <EditableTimer
+              id="2"
+              title="Bake squash"
+              project="Kitchen Chores"
+              elapsed="3890985"
+              editFormOpen
+            />
+          </ScrollView>
+          </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -108,6 +79,22 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  appContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
+  }, title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
   },
 });
 
